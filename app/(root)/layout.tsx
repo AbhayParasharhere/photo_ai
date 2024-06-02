4a655dd2813f1e05edb84c3fe8aae6b340fffe90
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2024-06-02 12:05:38 -0700

    Finish layout for sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,13 @@
+import SideBar from "@/components/shared/sidebar/sidebar";
+
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <main className="root">
       <div className="root--container">
-        <div className="wrapper">RootLayout{children}</div>
+        <div className="wrapper">
+          <SideBar />
+          {children}
+        </div>
       </div>
     </main>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,8 @@ import {UserButton} from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <p>Home</p>
-      <UserButton afterSignOutUrl="/" />
+      {/* <p>Home</p>
+      <UserButton afterSignOutUrl="/" /> */}
     </div>
   );
 }
